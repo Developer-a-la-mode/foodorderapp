12f8a60477301893cb4bf9195ce6502c49b85643
Kevin Tonthat <tonthat.kevin@gmail.com>
2022-08-11 00:41:34 -0700

    Header component

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = () => {
+
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h1>FoodApp</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt='A table full of delicous food!' />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
